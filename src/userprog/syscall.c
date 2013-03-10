73d3cf1110c5868d83dd6079aa7674530d91b177
Doug <ddg625@cs.utexas.edu>
2013-03-10 16:10:36 -0500

    write kinda implemented

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -4,6 +4,7 @@
 #include "threads/interrupt.h"
 #include "threads/thread.h"
 #include <filesys/file.h>
+#include "threads/vaddr.h"
 
 static void syscall_handler (struct intr_frame *);
 
@@ -16,8 +17,24 @@ syscall_init (void)
 static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
-  printf ("system call!\n");
-  thread_exit ();
+  hex_dump((int) f->esp, f->esp, (char *) PHYS_BASE - (char *) f->esp, 1);
+  int call_number = *(int *) f->esp;
+  void *arg0 = f->esp + 4;
+  void *arg1 = f->esp + 8;
+  void *arg2 = f->esp + 12;
+  
+  switch (call_number)
+  {
+    case SYS_WRITE:
+      write (*(int *) arg0, arg1, *(unsigned *) arg2);
+      break;
+    default:
+      printf("system call!\n");
+      thread_exit();
+      break;      
+  }
+  
+  thread_exit(); 
 }
 
 /*
@@ -49,70 +66,28 @@ int write (int fd, const void *buffer, unsigned size)
 {
   struct thread *t = thread_current();
   struct file *f = list_entry (list_begin (&t->open_files), struct file, open_file);
-  
-  while ((int) &f->fd != fd);
+  if (fd == 0) // stdin
   {
-    if( &f->open_file == list_tail(&t->open_files) )
-    {
-      return -1;
-    }
-    f = list_entry (list_next(&f->open_file), struct file, open_file);
+    return 0;
   }
+  else if (fd == 1) // stdout
+  {
+    putbuf(*(int *)buffer, size);
+    return (int) size;
+  }
+  else // file
+  {
+    while ((int) &f->fd != fd);
+    { 
+      printf("this is looping: %d\n", &f->fd);
+      if( &f->open_file == list_tail(&t->open_files) )
+      {
+        return -1;
+      }
+      f = list_entry (list_next(&f->open_file), struct file, open_file);
+    }
+  }
+  //printf("testing: %x\n", *(int *)buffer);
   
-  
-  
-  return 0;
+  //return (int) file_write(f, buffer, size);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
