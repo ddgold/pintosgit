a218f9f1bf86338fbb5e0c6e2fa38bff4f341d8e
Kelby Ludwig <kkl435@cs.utexas.edu>
2013-03-22 23:17:40 -0500

    rox simple working

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -417,7 +417,14 @@ load (const char *file_name, void (**eip) (void), void **esp)
   /* End ADDED */
  
   /* We arrive here whether the load is successful or not. */
-  file_close (file);
+  if (success)
+  {
+    file_deny_write(file);
+  }
+  else
+  {
+    file_close (file);
+  }
   return success;
 }
 
