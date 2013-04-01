2f08a3b9d53c66eae028ef0c8be955d87de223da
Kelby Ludwig <kkl435@cs.utexas.edu>
2013-03-31 22:16:05 -0500

    comments and stuff to frame_table_add

--- src/vm/frame.c
+++ src/vm/frame.c
@@ -18,8 +18,8 @@ void frame_table_init (void)
     void *index = palloc_get_page(PAL_USER);//allocating a page in PHY_MEM
     fm.p_addr = &index; //Sets the physical address of the frame in the table
     fm.isMapped = 0;    //initially the frame is unmapped
-    struct frame *fm_ptr = &fm;
-    list_push_back(&frame_table, &fm_ptr->frame_elem);
+    struct frame *fm_ptr = &fm; //sets the virtual address in the frame table
+    list_push_back(&frame_table, &fm_ptr->frame_elem); 
   }
 }
 
@@ -33,10 +33,22 @@ uint32_t* frame_table_add (struct page *pg)
   {
     struct list_elem *e;
     struct frame *fm;
+    
     for(e = list_begin(&frame_table); e != list_end(&frame_table);
         e = list_next(e))
     {
+      //Get a frame from the frame table
       fm = list_entry(e, struct frame, frame_elem);
+      //finds the first unmapped frame in the table
+      if(fm->isMapped == 0)
+      {
+        //Copy the page into physical memory
+        memcpy(&fm->p_addr, &pg, sizeof(struct page));
+        //Set the mapped bit
+        fm->isMapped = 1;
+        //Decrement the amount of open pages left count
+        --frame_table_left;
+      }
     }
   }
   else
